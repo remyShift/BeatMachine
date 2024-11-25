a3ebceb3f90995f437007b1d2727f607b9a8aa8b
Remy Cassagne <remy.engassac@icloud.com>
2024-11-25 15:36:24 +0100

    'done'

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,1 +1,22 @@
+const manifest = {
+    "name": "BeatMachine",
+    "short_name": "BeatMachine",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#ffffff",
+    "theme_color": "#000000",
+    "icons": [
+        {
+        "src": "/images/Bass.png",
+        "sizes": "192x192",
+        "type": "image/png"
+        },
+        {
+        "src": "/images/Bass1.png",
+        "sizes": "512x512",
+        "type": "image/png"
+        }
+    ]
+};
 
+export default manifest;
