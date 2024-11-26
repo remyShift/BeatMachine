529ae8970630b326084ccf47d5b886d8a4de2ee6
Remy Cassagne <remy.engassac@icloud.com>
2024-11-26 16:10:11 +0100

    edit landing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_153903) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_26_142519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_153903) do
     t.integer "bpm"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "genre"
   end
 
   create_table "pad_drumrack_samples", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,44 +17,38 @@ sample.save
 sound_file.close
 
 
-# Creates drumracks
-# Create first drumrack
-drumrack = Drumrack.new(name: "My tune Reggaetown", bpm: 100)
+genres = ["rock", "jazz", "rap", "pop", "funk", "hip-hop"]
+drumrack_names = ["Funky Beats", "Rock Solid", "Jazz Fusion", "Pop Hits", "Rap Attack", "Groove Master"]
+bpm_range = (80..160)
 
-16.times do  |i|
-  pad = Pad.new(step: i + 1)
-  pad.drumrack = drumrack
- if (i + 1)%4 == 0
-  pad.samples << sample
- end
-  pad.save
-end
+# Création de 6 drumracks
+genres.each_with_index do |genre, index|
+  # Création d'un nouveau drumrack avec des valeurs aléatoires
+  drumrack = Drumrack.new(
+    name: drumrack_names[index],
+    bpm: bpm_range.to_a.sample,
+    genre: genre
+  )
 
-drumrack.save
-
-# Create second drumrack
-drumrack = Drumrack.new(name: "Naive new beaters", bpm: 120)
-
-16.times do  |i|
-  pad = Pad.new(step: i + 1)
-  pad.drumrack = drumrack
-  if (i + 1)%3 == 0
-    pad.samples << sample
-  end
+  # Création des 16 pads pour chaque drumrack
+  16.times do |i|
+    pad = Pad.new(step: i + 1)
+    pad.drumrack = drumrack
+    # Ajout aléatoire du sample (1 chance sur 3)
+    if rand(3) == 0
+      pad.samples << sample
+    end
     pad.save
+  end
+
+  drumrack.save
 end
-
-
-drumrack.save
 
 # Prints number of samples
 p "Created #{Sample.count} samples"
 
 # Prints number of drumracks
 p "Created #{Drumrack.count} drumracks"
-
-# Prints number of pads
-p "#{drumrack.pads.count} pads created for #{drumrack.name} drumrack"
 
 # Prints number of pads sample was attached to
 p "Sample kick was added to #{sample.pads.count} pads"
