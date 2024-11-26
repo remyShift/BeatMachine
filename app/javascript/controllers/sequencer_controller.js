364fb1701ecfa5a68d556bca2188cc83900f3ea4
Remi Roset <remi.roset.pro@gmail.com>
2024-11-26 15:19:25 +0100

    nouvelle organisation du code JS

---
+++ app/javascript/controllers/sequencer_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+import * as Tone from "tone"
+
+// Connects to data-controller="sequencer"
+export default class extends Controller {
+  connect() {
+    console.log("lezgong")
+  }
+}
