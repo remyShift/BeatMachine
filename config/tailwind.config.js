dccbbfa89a67553eda8138b186ddb59c2865d245
Remy Cassagne <remy.engassac@icloud.com>
2024-11-26 14:38:46 +0100

    'done'

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,6 +19,9 @@ module.exports = {
         darkblue: '#2a2a72',
         darkgrey: '#232528',
       },
+      boxShadow: {
+        'top': '0 -4px 6px -1px rgba(0, 0, 0, 0.1)',
+      },
     },
   },
   plugins: [
