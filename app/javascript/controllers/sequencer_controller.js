d78a7ac8e9b59e508d93d25fcd7c6a9b172c9069
Remy Cassagne <remy.engassac@icloud.com>
2024-11-27 14:39:43 +0100

    drumrack edit

--- app/javascript/controllers/sequencer_controller.js
+++ app/javascript/controllers/sequencer_controller.js
@@ -3,10 +3,17 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="sequencer"
 export default class extends Controller {
-  static values = { bpm: Number };
+  static values = { bpm: Number, samples: Object };
   static targets = ["pad"];
 
   connect() {
+    this.audioElements = {
+      bass: new Audio(this.samplesValue["bass"]),
+      snare: new Audio(this.samplesValue["snare"]), 
+      hihat: new Audio(this.samplesValue["hihat"]),
+      kick: new Audio(this.samplesValue["kick"]),
+      oneshot: new Audio(this.samplesValue["oneshot"])
+    };
   }
 
   play() {
@@ -16,12 +23,23 @@ export default class extends Controller {
       this.padTargets.forEach((pad) => {
         pad.classList.remove("active");
       });
+
       const pad = document.querySelector("#pad-" + i);
       pad.classList.add("active");
 
+      console.log(pad.dataset.samples);
+
+      JSON.parse(pad.dataset.samples).forEach((sample_str) => {
+        const sample = JSON.parse(sample_str);
+        console.log(sample);
+        if (sample.active) {
+          this.audioElements[sample.category].play();
+        }
+      });
+
+
       if (i % 4 === 0) {
-        const audio = new Audio(pad.dataset.sample);
-        audio.play();
+
       }
 
       i === 15 ? (i = 0) : i++;
