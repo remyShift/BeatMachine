fe5724d0a04f2a9fbb67fbbc5ce6b3f34c2f03fd
Remy Cassagne <remy.engassac@icloud.com>
2024-11-29 15:56:54 +0100

    'done'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Rails.application.configure do
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
+  config.active_storage.service = :cloudinary
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
