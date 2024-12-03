0f856e9ada42fe654e7ee4f2b54c3a2adcba1d38
Remi Roset <remi.roset.pro@gmail.com>
2024-12-03 12:29:31 +0100

    gradient done

--- app/helpers/genre_cards_helper.rb
+++ app/helpers/genre_cards_helper.rb
@@ -5,8 +5,6 @@ module GenreCardsHelper
       "bg-gradient-to-r from-primary to-yelloww"
     when "jerseyclub"
       "bg-gradient-to-r from-blue to-lightblue"
-    when "pop"
-      "bg-gradient-to-r from-pink to-orangee"
     when "bailefunk"
       "bg-gradient-to-r from-pink to-orangee"
     when "trap"
@@ -15,8 +13,6 @@ module GenreCardsHelper
       "bg-gradient-to-r from-emerald-700 to-green-500"
     when "jungle"
       "bg-gradient-to-r from-purple to-lightpurple"
-    when "techno"
-      "bg-gradient-to-r from-green-700 to-green-400"
     else
       "bg-gray-500"
     end
