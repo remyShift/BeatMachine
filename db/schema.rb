8b9eb2d5a9d515cb107ec4747ce681e3e14e36b8
Lea Gau <157475289+lea3738@users.noreply.github.com>
2024-12-03 18:00:26 +0100

    help templates

--- db/schema.rb
+++ db/schema.rb
@@ -96,15 +96,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_02_161907) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "solid_cable_messages", force: :cascade do |t|
-    t.text "channel"
-    t.text "payload"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["channel"], name: "index_solid_cable_messages_on_channel"
-    t.index ["created_at"], name: "index_solid_cable_messages_on_created_at"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
